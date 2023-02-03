34ba7e4bd6e83fc7efd444415fe0c9a6361044db
Jake Brunner <Jbbrunner10@gmail.com>
2023-02-03 08:26:25 -0800

    debugging port issue

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
 
