23e806ea4e640d3652ec0ec996146bb15639211f
Jake Brunner <Jbbrunner10@gmail.com>
2023-02-03 11:50:09 -0800

    simulate working project via refactoring

--- app.js
+++ app.js
@@ -2,27 +2,34 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const _ = require("lodash");
 const mongoose = require("mongoose");
+const _ = require("lodash");
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-//connect Mongoose
-const mongoAtlasKey = process.env.MONGO_ATLAS_KEY;
-mongoose.connect(mongoAtlasKey, {useNewUrlParser: true});
 
-// Post Schema
-const postSchema = {title: String, content: String};
-// Pose Model
-const Post = mongoose.model("Post", postSchema);
-
-// connect and set express app, ejs view,  and body parse
+// connect and set express app, ejs view,  and body parser
+// set app to express
 const app = express();
-app.set('view engine', 'ejs');
+// set EJS view engine
+app.set("view engine", "ejs");
+
+const mongoAtlasKey = process.env.MONGO_ATLAS_KEY;
+
+// connect body-parser, public folder
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+
+// connect to database server
+mongoose.connect(mongoAtlasKey, {useNewUrlParser: true});
+// Post Schema
+const postSchema = {title: String, content: String};
+// Post Model
+const Post = mongoose.model("Post", postSchema);
 
 // load Homepage, get all posts from DB
 app.get("/", function(req, res){
@@ -74,8 +81,16 @@ app.get("/posts/:postId", function(req, res){
  });
 });
 
-// check for server status
-app.set('port', process.env.PORT || 3000);
-app.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-  });
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started succesfully");
+});
+
+// redirect to about page
+app.get(port, function(req, res){
+  res.render("about");
+});
