862038b2adc970984a29159cd7cf19e89eddd737
Jake Brunner <Jbbrunner10@gmail.com>
2023-02-03 08:42:46 -0800

    edit port

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.get("/posts/:postId", function(req, res){
 });
 
 // check for server status
-var port = process.env.port || 3000;
-app.listen(port, function() {
-  console.log("Server started on port 3000");
-});
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+  });
